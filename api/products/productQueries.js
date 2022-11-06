213fe1886ab8749cc35e73261de7083f34c36a95
Blaise Ndizeye <khalifablaise@gmail.com>
2022-11-06 18:18:56 +0200

    Implement getProductData for the owners of the product

--- api/products/productQueries.js
+++ api/products/productQueries.js
@@ -1,3 +1,5 @@
+const { ApolloError } = require("apollo-server-errors")
+
 const Product = require("../../models/Product")
 const { generateServerError } = require("../../helpers/errorHelpers")
 const {
@@ -20,6 +22,23 @@ const productQueries = {
       let allProducts = await Product.find().sort({ _id: -1 })
 
       return allProducts.map((product) => productData(product))
+    } catch (err) {
+      generateServerError(err)
+    }
+  },
+  async GetProductData(_, { user_id, product_id }, ctx, ___) {
+    try {
+      isAuthenticated(ctx)
+      isValidUser(ctx.user, user_id)
+      isAccountVerified(ctx.user)
+      isPayingUser(ctx.user)
+
+      const productExists = await Product.findOne({
+        $and: [{ _id: product_id }, { user_id }],
+      })
+      if (!productExists) throw new ApolloError("Product not found", 404)
+
+      return productData(productExists)
     } catch (err) {
       generateServerError(err)
     }
