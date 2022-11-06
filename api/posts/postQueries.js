6500b5682ec0e807f6eac6a0a2aafec3fee23fac
Blaise Ndizeye <khalifablaise@gmail.com>
2022-11-06 18:28:25 +0200

    Implement getPostData for the owner of the post

--- api/posts/postQueries.js
+++ api/posts/postQueries.js
@@ -1,3 +1,5 @@
+const { ApolloError } = require("apollo-server-errors")
+
 const Post = require("../../models/Post")
 const { generateServerError } = require("../../helpers/errorHelpers")
 const {
@@ -20,6 +22,23 @@ const postQueries = {
       const allPosts = await Post.find().sort({ _id: -1 })
 
       return allPosts.map((post) => postData(post))
+    } catch (err) {
+      generateServerError(err)
+    }
+  },
+  async GetPostData(_, { user_id, post_id }, ctx, ___) {
+    try {
+      isAuthenticated(ctx)
+      isValidUser(ctx.user, user_id)
+      isAccountVerified(ctx.user)
+      isPayingUser(ctx.user)
+
+      const postExists = await Post.findOne({
+        $and: [{ _id: post_id }, { user_id }],
+      })
+      if (!postExists) throw new ApolloError("Post not found", 404)
+
+      return postData(postExists)
     } catch (err) {
       generateServerError(err)
     }
